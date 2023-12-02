f7a682216921ada94caea8319296554eb28c16ac
Elgar17 <1653794708@qq.com>
2023-12-02 20:43:07 +0800

    auto commit

---
+++ .server/215.app.js
@@ -0,0 +1,147 @@
+"use strict";
+exports.id = 215;
+exports.ids = [215];
+exports.modules = {
+
+/***/ 7884:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "data": () => (/* binding */ data)
+/* harmony export */ });
+const data = {
+  "key": "v-07588cce",
+  "path": "/learn.html",
+  "title": "学习哈萨克语",
+  "lang": "zh",
+  "frontmatter": {
+    "sidebar": false
+  },
+  "excerpt": "",
+  "headers": [
+    {
+      "level": 2,
+      "title": "电子书",
+      "slug": "电子书",
+      "children": []
+    },
+    {
+      "level": 2,
+      "title": "文章",
+      "slug": "文章",
+      "children": []
+    },
+    {
+      "level": 2,
+      "title": "电影",
+      "slug": "电影",
+      "children": []
+    }
+  ],
+  "filePathRelative": "learn.md",
+  "git": {
+    "updatedTime": 1665759226000,
+    "contributors": [
+      {
+        "name": "Elgar17",
+        "email": "1653794708@qq.com",
+        "commits": 2
+      },
+      {
+        "name": "elgar17",
+        "email": "1653794708@qq.com",
+        "commits": 2
+      },
+      {
+        "name": "Elgar",
+        "email": "45301805+Elgar17@users.noreply.github.com",
+        "commits": 1
+      }
+    ]
+  }
+}
+
+
+/***/ }),
+
+/***/ 15228:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ learn_html)
+});
+
+// EXTERNAL MODULE: external "vue"
+var external_vue_ = __webpack_require__(21666);
+// EXTERNAL MODULE: ./node_modules/vue/server-renderer/index.mjs
+var server_renderer = __webpack_require__(14498);
+;// CONCATENATED MODULE: ./node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[2]!./node_modules/@vuepress/bundler-webpack/lib/build/ssr/vuepressLoader.js!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[1]!./docs/.vuepress/.temp/pages/learn.html.vue?vue&type=template&id=0cc58c62
+
+
+
+function ssrRender(_ctx, _push, _parent, _attrs) {
+  const _component_OutboundLink = (0,external_vue_.resolveComponent)("OutboundLink")
+  const _component_Valine = (0,external_vue_.resolveComponent)("Valine")
+
+  _push(`<!--[--><h1 id="学习哈萨克语" tabindex="-1"><a class="header-anchor" href="#学习哈萨克语" aria-hidden="true">#</a> 学习哈萨克语</h1><p>这里会收集一些网上的学习哈萨克语相关的资料，如果你有什么好的资料欢迎联系我<a href="https://support.qq.com/products/369710" target="_blank" rel="noopener noreferrer">投稿`)
+  _push((0,server_renderer.ssrRenderComponent)(_component_OutboundLink, null, null, _parent))
+  _push(`</a>！</p><h2 id="电子书" tabindex="-1"><a class="header-anchor" href="#电子书" aria-hidden="true">#</a> 电子书</h2><ol><li><a href="https://abaialemi.kz/kz/post/view?id=668" target="_blank" rel="noopener noreferrer">《阿拜之路（1）》`)
+  _push((0,server_renderer.ssrRenderComponent)(_component_OutboundLink, null, null, _parent))
+  _push(`</a></li></ol><h2 id="文章" tabindex="-1"><a class="header-anchor" href="#文章" aria-hidden="true">#</a> 文章</h2><ol><li><a href="https://elgara.gitee.io/demo/hozi/#/form" target="_blank" rel="noopener noreferrer">哈萨克语字母表`)
+  _push((0,server_renderer.ssrRenderComponent)(_component_OutboundLink, null, null, _parent))
+  _push(`</a></li><li><a href="https://www.zhihu.com/question/65270788/answer/325188906" target="_blank" rel="noopener noreferrer">如何评价哈萨克斯坦拉丁化哈萨克文字母表？`)
+  _push((0,server_renderer.ssrRenderComponent)(_component_OutboundLink, null, null, _parent))
+  _push(`</a></li><li><a href="https://mp.weixin.qq.com/s/pDnPV-Mzezn3qZhZZlwrwA" target="_blank" rel="noopener noreferrer">قازاق ءتىلىنىڭ ەملە ەرەجەسى`)
+  _push((0,server_renderer.ssrRenderComponent)(_component_OutboundLink, null, null, _parent))
+  _push(`</a></li></ol><h2 id="电影" tabindex="-1"><a class="header-anchor" href="#电影" aria-hidden="true">#</a> 电影</h2><ol><li><a href="https://mp.weixin.qq.com/s/dKSNYprmI0nCf1nG6faOBA" target="_blank" rel="noopener noreferrer">جاۋ جۇرەك مىڭ بالا - 铁血一千勇士`)
+  _push((0,server_renderer.ssrRenderComponent)(_component_OutboundLink, null, null, _parent))
+  _push(`</a></li><li><a href="https://mp.weixin.qq.com/s/12BiSQF1sCZveY3B1Qnfgw" target="_blank" rel="noopener noreferrer">جانىبەك حان مەن كەرەي حان - 哈萨克汗国`)
+  _push((0,server_renderer.ssrRenderComponent)(_component_OutboundLink, null, null, _parent))
+  _push(`</a></li><li><a href="https://mp.weixin.qq.com/s/7tWEpZ3m3_c3ADl5uEG1dg" target="_blank" rel="noopener noreferrer">شال`)
+  _push((0,server_renderer.ssrRenderComponent)(_component_OutboundLink, null, null, _parent))
+  _push(`</a></li></ol>`)
+  _push((0,server_renderer.ssrRenderComponent)(_component_Valine, null, null, _parent))
+  _push(`<!--]-->`)
+}
+;// CONCATENATED MODULE: ./docs/.vuepress/.temp/pages/learn.html.vue?vue&type=template&id=0cc58c62
+
+// EXTERNAL MODULE: ./node_modules/vue-loader/dist/exportHelper.js
+var exportHelper = __webpack_require__(83744);
+;// CONCATENATED MODULE: ./docs/.vuepress/.temp/pages/learn.html.vue
+
+const script = {}
+
+;
+const __exports__ = /*#__PURE__*/(0,exportHelper/* default */.Z)(script, [['ssrRender',ssrRender]])
+
+/* harmony default export */ const learn_html = (__exports__);
+
+/***/ }),
+
+/***/ 83744:
+/***/ ((__unused_webpack_module, exports) => {
+
+var __webpack_unused_export__;
+
+__webpack_unused_export__ = ({ value: true });
+// runtime helper for setting properties on components
+// in a tree-shakable way
+exports.Z = (sfc, props) => {
+    const target = sfc.__vccOpts || sfc;
+    for (const [key, val] of props) {
+        target[key] = val;
+    }
+    return target;
+};
+
+
+/***/ })
+
+};
+;
+//# sourceMappingURL=215.app.js.map
---
+++ .server/511.app.js
@@ -0,0 +1,89 @@
+"use strict";
+exports.id = 511;
+exports.ids = [511];
+exports.modules = {
+
+/***/ 10540:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "data": () => (/* binding */ data)
+/* harmony export */ });
+const data = {
+  "key": "v-2d35b3f9",
+  "path": "/blog/2-kz321-site.html",
+  "title": "曾经的 KZ321.COM",
+  "lang": "zh",
+  "frontmatter": {
+    "sidebar": false
+  },
+  "excerpt": "",
+  "headers": [],
+  "filePathRelative": "blog/2-kz321-site.md",
+  "git": {
+    "updatedTime": 1701520911000,
+    "contributors": [
+      {
+        "name": "Elgar17",
+        "email": "1653794708@qq.com",
+        "commits": 1
+      }
+    ]
+  }
+}
+
+
+/***/ }),
+
+/***/ 28301:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ _2_kz321_site_html)
+});
+
+;// CONCATENATED MODULE: ./node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[2]!./node_modules/@vuepress/bundler-webpack/lib/build/ssr/vuepressLoader.js!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[1]!./docs/.vuepress/.temp/pages/blog/2-kz321-site.html.vue?vue&type=template&id=7024ab22
+
+function ssrRender(_ctx, _push, _parent, _attrs) {
+  _push(`<!--[--><h1 id="曾经的-kz321-com" tabindex="-1"><a class="header-anchor" href="#曾经的-kz321-com" aria-hidden="true">#</a> 曾经的 KZ321.COM</h1><p>2010 年左右，我刚接触互联网，感觉打开了新世界的大门。在那时一些流行的网站，现在已经无法访问了，这里记录一些曾经访问过的网站，里面我最为常用的是 kz321 导航网站。</p><p>今天我找到一个能查看网站历史的工具，找到了当年的 kz321 导航网站。</p><p><img src="/home-kz321.PNG" alt="kz321"></p><p><img src="/about-kz321.PNG" alt="kz321.com"></p><!--]-->`)
+}
+// EXTERNAL MODULE: ./node_modules/vue-loader/dist/exportHelper.js
+var exportHelper = __webpack_require__(83744);
+;// CONCATENATED MODULE: ./docs/.vuepress/.temp/pages/blog/2-kz321-site.html.vue
+
+const script = {}
+
+;
+const __exports__ = /*#__PURE__*/(0,exportHelper/* default */.Z)(script, [['ssrRender',ssrRender]])
+
+/* harmony default export */ const _2_kz321_site_html = (__exports__);
+
+/***/ }),
+
+/***/ 83744:
+/***/ ((__unused_webpack_module, exports) => {
+
+var __webpack_unused_export__;
+
+__webpack_unused_export__ = ({ value: true });
+// runtime helper for setting properties on components
+// in a tree-shakable way
+exports.Z = (sfc, props) => {
+    const target = sfc.__vccOpts || sfc;
+    for (const [key, val] of props) {
+        target[key] = val;
+    }
+    return target;
+};
+
+
+/***/ })
+
+};
+;
+//# sourceMappingURL=511.app.js.map
