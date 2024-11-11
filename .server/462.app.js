dd7970c5511a762787c111517d3ce8dd1c92ec26
Elgar17 <1653794708@qq.com>
2024-11-12 00:07:19 +0800

    auto commit

---
+++ .server/462.app.js
@@ -0,0 +1,158 @@
+"use strict";
+exports.id = 462;
+exports.ids = [462];
+exports.modules = {
+
+/***/ 2462:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ Alphabet)
+});
+
+// EXTERNAL MODULE: external "vue"
+var external_vue_ = __webpack_require__(21666);
+// EXTERNAL MODULE: ./node_modules/vue/server-renderer/index.mjs
+var server_renderer = __webpack_require__(14498);
+;// CONCATENATED MODULE: ./node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[2]!./node_modules/@vuepress/bundler-webpack/lib/build/ssr/vuepressLoader.js!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[1]!./docs/.vuepress/components/Alphabet.vue?vue&type=template&id=d7841580&scoped=true
+
+
+
+function ssrRender(_ctx, _push, _parent, _attrs, $props, $setup, $data, $options) {
+  _push(`<div${(0,server_renderer.ssrRenderAttrs)((0,external_vue_.mergeProps)({ class: "container" }, _attrs))} data-v-d7841580><h1 data-v-d7841580>الپپە</h1><div class="card-grid" data-v-d7841580><!--[-->`)
+  ;(0,server_renderer.ssrRenderList)($data.alphabet, (item) => {
+    _push(`<div class="card" data-v-d7841580><div class="card-content" data-v-d7841580><div class="letter-container" data-v-d7841580><span class="letter" data-v-d7841580>${
+      (0,server_renderer.ssrInterpolate)(item.letter)
+    }</span></div><div class="card-footer" data-v-d7841580><div data-v-d7841580></div><div class="word-container" data-v-d7841580><span class="emoji"${
+      (0,server_renderer.ssrRenderAttr)("aria-label", `Emoji for ${item.word}`)
+    } data-v-d7841580>${
+      (0,server_renderer.ssrInterpolate)(item.emoji)
+    }</span><span class="word" data-v-d7841580>${
+      (0,server_renderer.ssrInterpolate)(item.word)
+    }</span></div></div></div></div>`)
+  })
+  _push(`<!--]--></div></div>`)
+}
+;// CONCATENATED MODULE: ./docs/.vuepress/components/Alphabet.vue?vue&type=template&id=d7841580&scoped=true
+
+;// CONCATENATED MODULE: ./node_modules/@vuepress/bundler-webpack/lib/build/ssr/vuepressLoader.js!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[1]!./docs/.vuepress/components/Alphabet.vue?vue&type=script&lang=js
+
+/* harmony default export */ const Alphabetvue_type_script_lang_js = ({
+  data() {
+    return {
+      alphabet: [
+        { letter: "ا", word: "الما", emoji: "🍎", isPlaying: false },
+        { letter: "ءا", word: "اتەش", emoji: "🐓", isPlaying: false },
+        { letter: "ب", word: "بانان", emoji: "🍌", isPlaying: false },
+        { letter: "ۆ", word: "ۆەلوسيپەد", emoji: "🚴🏻", isPlaying: false },
+        { letter: "گ", word: "گۇل", emoji: "💐", isPlaying: false },
+        { letter: "ع", word: "عالىم", emoji: "👨‍🔬", isPlaying: false },
+        { letter: "د", word: "دوپ", emoji: "⚽", isPlaying: false },
+        { letter: "ە", word: "ەسىك", emoji: "🚪", isPlaying: false },
+        { letter: "ج", word: "جولبارىس", emoji: "🐅", isPlaying: false },
+        { letter: "ز", word: "زەبرا", emoji: "🦓", isPlaying: false },
+        { letter: "ي", word: "يت", emoji: "🐕", isPlaying: false },
+        { letter: "ك", word: "كوبەلەك", emoji: "🦋", isPlaying: false },
+        { letter: "ق", word: "قويان", emoji: "🐰", isPlaying: false },
+        { letter: "ل", word: "ليمون", emoji: "🍋", isPlaying: false },
+        { letter: "م", word: "مىسىق", emoji: "🐈", isPlaying: false },
+        { letter: "ن", word: "نار", emoji: "🐪", isPlaying: false },
+        { letter: "ڭ", word: "", emoji: "", isPlaying: false },
+        { letter: "و", word: "ورىندىق", emoji: "🪑", isPlaying: false },
+        { letter: "ءو", word: "ورمەكشى", emoji: "🕷️", isPlaying: false },
+        { letter: "پ", word: "ءپىل", emoji: "🐘", isPlaying: false },
+        { letter: "ر", word: "راديو", emoji: "📻", isPlaying: false },
+        { letter: "س", word: "ساعات", emoji: "⏰", isPlaying: false },
+        { letter: "ت", word: "تاس", emoji: "🪨", isPlaying: false },
+        { letter: "ۋ", word: "ۋىق", emoji: "", isPlaying: false },
+        { letter: "ۇ", word: "ۇلۋ", emoji: "🐌", isPlaying: false },
+        { letter: "ءۇ", word: "ءۇي", emoji: "🏠", isPlaying: false },
+        { letter: "ف", word: "فونتان", emoji: "⛲", isPlaying: false },
+        { letter: "ح", word: "حات", emoji: "📨", isPlaying: false },
+        { letter: "ھ", word: "ايداھار", emoji: "🐲", isPlaying: false },
+        { letter: "چ", word: "چەك", emoji: "🎫", isPlaying: false },
+        { letter: "ش", word: "شار", emoji: "🎈", isPlaying: false },
+        { letter: "ى", word: "ىدىس", emoji: "🥣", isPlaying: false },
+        { letter: "ءى", word: "ءىز", emoji: "👣", isPlaying: false },
+      ]
+    }
+  },
+  methods: {
+    playPronunciation(item) {
+      item.isPlaying = true;
+      // In a real implementation, you would trigger audio playback here
+      // For this example, we'll just simulate it with a timeout
+      setTimeout(() => {
+        item.isPlaying = false;
+      }, 1000);
+    }
+  }
+});
+
+;// CONCATENATED MODULE: ./docs/.vuepress/components/Alphabet.vue?vue&type=script&lang=js
+ 
+// EXTERNAL MODULE: ./node_modules/css-loader/dist/cjs.js??clonedRuleSet-34.use[0]!./node_modules/vue-loader/dist/stylePostLoader.js!./node_modules/postcss-loader/dist/cjs.js??clonedRuleSet-34.use[1]!./node_modules/@vuepress/bundler-webpack/lib/build/ssr/vuepressLoader.js!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[1]!./docs/.vuepress/components/Alphabet.vue?vue&type=style&index=0&id=d7841580&scoped=true&lang=css
+var Alphabetvue_type_style_index_0_id_d7841580_scoped_true_lang_css = __webpack_require__(43228);
+;// CONCATENATED MODULE: ./docs/.vuepress/components/Alphabet.vue?vue&type=style&index=0&id=d7841580&scoped=true&lang=css
+
+// EXTERNAL MODULE: ./node_modules/vue-loader/dist/exportHelper.js
+var exportHelper = __webpack_require__(83744);
+;// CONCATENATED MODULE: ./docs/.vuepress/components/Alphabet.vue
+
+
+
+
+;
+
+
+const __exports__ = /*#__PURE__*/(0,exportHelper/* default */.Z)(Alphabetvue_type_script_lang_js, [['ssrRender',ssrRender],['__scopeId',"data-v-d7841580"]])
+
+/* harmony default export */ const Alphabet = (__exports__);
+
+/***/ }),
+
+/***/ 43228:
+/***/ ((module, __unused_webpack___webpack_exports__, __webpack_require__) => {
+
+/* harmony import */ var _node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(87537);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(23645);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);
+// Imports
+
+
+var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));
+// Module
+___CSS_LOADER_EXPORT___.push([module.id, "@keyframes pulse-d7841580{0%,to{opacity:1}50%{opacity:.5}}.container[data-v-d7841580]{max-width:1200px;margin:0 auto;padding:20px}h1[data-v-d7841580]{font-size:2rem;font-weight:700;text-align:center;margin-bottom:2rem}.card-grid[data-v-d7841580]{display:grid;grid-template-columns:repeat(auto-fill,minmax(150px,1fr));gap:1rem}.card[data-v-d7841580]{background-color:#fff;border-radius:8px;box-shadow:0 2px 4px rgba(0,0,0,.1);overflow:hidden}.card-content[data-v-d7841580]{padding:1rem;display:flex;flex-direction:column;height:100%}.letter-container[data-v-d7841580]{display:flex;align-items:center;justify-content:center;background-color:#f0f0f0;border-radius:4px;margin-bottom:1rem;padding:15px 0}.letter[data-v-d7841580]{font-size:4rem;font-weight:700;color:#333}.card-footer[data-v-d7841580]{justify-content:space-between}.card-footer[data-v-d7841580],.word-container[data-v-d7841580]{display:flex;align-items:center}.word[data-v-d7841580]{font-size:1rem;font-weight:600;margin-left:.5rem}.emoji[data-v-d7841580]{font-size:2rem}button[data-v-d7841580]{background:0 0;border:0;cursor:pointer;padding:.5rem;border-radius:50%;transition:background-color .3s}button[data-v-d7841580]:hover{background-color:#f0f0f0}button[data-v-d7841580]:focus{outline:0;box-shadow:0 0 0 2px #e0e0e0}.icon[data-v-d7841580]{width:24px;height:24px;stroke:currentColor;stroke-width:2;stroke-linecap:round;stroke-linejoin:round;fill:none}.icon-playing[data-v-d7841580]{color:#4a90e2;animation:pulse-d7841580 2s cubic-bezier(.4,0,.6,1) infinite}@media (max-width:640px){.card-grid[data-v-d7841580]{grid-template-columns:repeat(2,1fr)}}@media (min-width:641px) and (max-width:1024px){.card-grid[data-v-d7841580]{grid-template-columns:repeat(3,1fr)}}@media (min-width:1025px){.card-grid[data-v-d7841580]{grid-template-columns:repeat(3,1fr)}}", "",{"version":3,"sources":["webpack://./docs/.vuepress/components/Alphabet.vue"],"names":[],"mappings":"AAyGA,0BACE,MACE,SACF,CACA,IACE,UACF,CACF,CA/GA,4BACE,gBAAiB,CACjB,aAAc,CACd,YACF,CAEA,oBACE,cAAe,CACf,eAAiB,CACjB,iBAAkB,CAClB,kBACF,CAEA,4BACE,YAAa,CACb,yDAA4D,CAC5D,QACF,CAEA,uBACE,qBAAuB,CACvB,iBAAkB,CAClB,mCAAwC,CACxC,eACF,CAEA,+BACE,YAAa,CACb,YAAa,CACb,qBAAsB,CACtB,WACF,CAEA,mCAEE,YAAa,CACb,kBAAmB,CACnB,sBAAuB,CACvB,wBAAyB,CACzB,iBAAkB,CAClB,kBAAmB,CACnB,cACF,CAEA,yBACE,cAAe,CACf,eAAiB,CACjB,UACF,CAEA,8BAEE,6BAEF,CAEA,+DACE,YAAa,CACb,kBACF,CAEA,uBACE,cAAe,CACf,eAAgB,CAChB,iBACF,CAEA,wBACE,cACF,CAEA,wBACE,cAAgB,CAChB,QAAY,CACZ,cAAe,CACf,aAAe,CACf,iBAAkB,CAClB,+BACF,CAEA,8BACE,wBACF,CAEA,8BACE,SAAa,CACb,4BACF,CAEA,uBACE,UAAW,CACX,WAAY,CACZ,mBAAoB,CACpB,cAAe,CACf,oBAAqB,CACrB,qBAAsB,CACtB,SACF,CAEA,+BACE,aAAc,CACd,4DACF,CAWA,yBACE,4BACE,mCACF,CACF,CAEA,gDACE,4BACE,mCACF,CACF,CAEA,0BACE,4BACE,mCACF,CACF","sourcesContent":["\n.container {\n  max-width: 1200px;\n  margin: 0 auto;\n  padding: 20px;\n}\n\nh1 {\n  font-size: 2rem;\n  font-weight: bold;\n  text-align: center;\n  margin-bottom: 2rem;\n}\n\n.card-grid {\n  display: grid;\n  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));\n  gap: 1rem;\n}\n\n.card {\n  background-color: white;\n  border-radius: 8px;\n  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);\n  overflow: hidden;\n}\n\n.card-content {\n  padding: 1rem;\n  display: flex;\n  flex-direction: column;\n  height: 100%;\n}\n\n.letter-container {\n  /* flex-grow: 1; */\n  display: flex;\n  align-items: center;\n  justify-content: center;\n  background-color: #f0f0f0;\n  border-radius: 4px;\n  margin-bottom: 1rem;\n  padding: 15px 0;\n}\n\n.letter {\n  font-size: 4rem;\n  font-weight: bold;\n  color: #333;\n}\n\n.card-footer {\n  display: flex;\n  justify-content: space-between;\n  align-items: center;\n}\n\n.word-container {\n  display: flex;\n  align-items: center;\n}\n\n.word {\n  font-size: 1rem;\n  font-weight: 600;\n  margin-left: 0.5rem;\n}\n\n.emoji {\n  font-size: 2rem;\n}\n\nbutton {\n  background: none;\n  border: none;\n  cursor: pointer;\n  padding: 0.5rem;\n  border-radius: 50%;\n  transition: background-color 0.3s;\n}\n\nbutton:hover {\n  background-color: #f0f0f0;\n}\n\nbutton:focus {\n  outline: none;\n  box-shadow: 0 0 0 2px #e0e0e0;\n}\n\n.icon {\n  width: 24px;\n  height: 24px;\n  stroke: currentColor;\n  stroke-width: 2;\n  stroke-linecap: round;\n  stroke-linejoin: round;\n  fill: none;\n}\n\n.icon-playing {\n  color: #4a90e2;\n  animation: pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite;\n}\n\n@keyframes pulse {\n  0%, 100% {\n    opacity: 1;\n  }\n  50% {\n    opacity: .5;\n  }\n}\n\n@media (max-width: 640px) {\n  .card-grid {\n    grid-template-columns: repeat(2, 1fr);\n  }\n}\n\n@media (min-width: 641px) and (max-width: 1024px) {\n  .card-grid {\n    grid-template-columns: repeat(3, 1fr);\n  }\n}\n\n@media (min-width: 1025px) {\n  .card-grid {\n    grid-template-columns: repeat(3, 1fr);\n  }\n}\n"],"sourceRoot":""}]);
+// Exports
+/* unused harmony default export */ var __WEBPACK_DEFAULT_EXPORT__ = ((/* unused pure expression or super */ null && (___CSS_LOADER_EXPORT___)));
+
+
+/***/ }),
+
+/***/ 83744:
+/***/ ((__unused_webpack_module, exports) => {
+
+var __webpack_unused_export__;
+
+__webpack_unused_export__ = ({ value: true });
+// runtime helper for setting properties on components
+// in a tree-shakable way
+exports.Z = (sfc, props) => {
+    const target = sfc.__vccOpts || sfc;
+    for (const [key, val] of props) {
+        target[key] = val;
+    }
+    return target;
+};
+
+
+/***/ })
+
+};
+;
+//# sourceMappingURL=462.app.js.map
---
+++ .server/915.app.js
@@ -0,0 +1,163 @@
+"use strict";
+exports.id = 915;
+exports.ids = [915];
+exports.modules = {
+
+/***/ 14915:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ InputSearch)
+});
+
+// EXTERNAL MODULE: external "vue"
+var external_vue_ = __webpack_require__(21666);
+// EXTERNAL MODULE: ./node_modules/vue/server-renderer/index.mjs
+var server_renderer = __webpack_require__(14498);
+// EXTERNAL MODULE: ./docs/.vuepress/public/kz321.png
+var kz321 = __webpack_require__(46846);
+;// CONCATENATED MODULE: ./node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[2]!./node_modules/@vuepress/bundler-webpack/lib/build/ssr/vuepressLoader.js!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[1]!./docs/.vuepress/components/InputSearch.vue?vue&type=template&id=1e438f06&scoped=true
+
+
+
+
+
+function ssrRender(_ctx, _push, _parent, _attrs, $props, $setup, $data, $options) {
+  _push(`<div${
+    (0,server_renderer.ssrRenderAttrs)((0,external_vue_.mergeProps)({ class: "input-search-wrap" }, _attrs))
+  } data-v-1e438f06><div class="logo-wrapper" data-v-1e438f06><img${
+    (0,server_renderer.ssrRenderAttr)("src", kz321)
+  } alt="" data-v-1e438f06></div><div class="input-search" data-v-1e438f06><ul class="ingin-wrapper" data-v-1e438f06><!--[-->`)
+  ;(0,server_renderer.ssrRenderList)($data.ingins, (item, index) => {
+    _push(`<li class="${
+      (0,server_renderer.ssrRenderClass)({ slected: $data.current == item })
+    }" data-v-1e438f06>${
+      (0,server_renderer.ssrInterpolate)(item)
+    }</li>`)
+  })
+  _push(`<!--]--></ul><input type="text"${
+    (0,server_renderer.ssrRenderAttr)("value", $data.input)
+  } placeholder="请输入" data-v-1e438f06><button data-v-1e438f06>搜索</button><span class="${
+    (0,server_renderer.ssrRenderClass)([{ heiden: $data.isHeiden }, "ctrld"])
+  }" data-v-1e438f06> ❤ Ctrl + D 收藏到导航</span></div></div>`)
+}
+;// CONCATENATED MODULE: ./docs/.vuepress/components/InputSearch.vue?vue&type=template&id=1e438f06&scoped=true
+
+;// CONCATENATED MODULE: ./node_modules/@vuepress/bundler-webpack/lib/build/ssr/vuepressLoader.js!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[1]!./docs/.vuepress/components/InputSearch.vue?vue&type=script&lang=js
+
+/* harmony default export */ const InputSearchvue_type_script_lang_js = ({
+  name: "InputSearch",
+  data () {
+    return {
+      current: "百度",
+      ingins: ["百度", "谷歌", "必应", "翻译"],
+      input: "",
+      isHeiden: false,
+      baidu: "https://www.baidu.com/s?ie=UTF-8&wd=",
+      google: "https://www.google.com/search?ie=utf-8&q=",
+      bing: "https://cn.bing.com/search?q=",
+      fanyi: "https://fanyi.baidu.com/translate#zh/en/"
+    };
+  },
+  mounted () {
+    this.time = setTimeout(() => {
+      this.isHeiden = true
+      clearTimeout(this.time)
+      this.time = null
+    }, 5000)
+  },
+  methods: {
+    search () {
+      if (this.input.trim() == "") return;
+      switch (this.current) {
+        case "百度":
+          window.open(this.baidu + this.input);
+          break;
+        case "谷歌":
+          window.open(this.google + this.input);
+          break;
+        case "必应":
+          window.open(this.bing + this.input);
+          break;
+        case "翻译":
+          window.open(this.fanyi + this.input);
+          break;
+      }
+    },
+  },
+});
+
+;// CONCATENATED MODULE: ./docs/.vuepress/components/InputSearch.vue?vue&type=script&lang=js
+ 
+// EXTERNAL MODULE: ./node_modules/css-loader/dist/cjs.js??clonedRuleSet-34.use[0]!./node_modules/vue-loader/dist/stylePostLoader.js!./node_modules/postcss-loader/dist/cjs.js??clonedRuleSet-34.use[1]!./node_modules/@vuepress/bundler-webpack/lib/build/ssr/vuepressLoader.js!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[1]!./docs/.vuepress/components/InputSearch.vue?vue&type=style&index=0&id=1e438f06&scoped=true&lang=css
+var InputSearchvue_type_style_index_0_id_1e438f06_scoped_true_lang_css = __webpack_require__(74928);
+;// CONCATENATED MODULE: ./docs/.vuepress/components/InputSearch.vue?vue&type=style&index=0&id=1e438f06&scoped=true&lang=css
+
+// EXTERNAL MODULE: ./node_modules/vue-loader/dist/exportHelper.js
+var exportHelper = __webpack_require__(83744);
+;// CONCATENATED MODULE: ./docs/.vuepress/components/InputSearch.vue
+
+
+
+
+;
+
+
+const __exports__ = /*#__PURE__*/(0,exportHelper/* default */.Z)(InputSearchvue_type_script_lang_js, [['ssrRender',ssrRender],['__scopeId',"data-v-1e438f06"]])
+
+/* harmony default export */ const InputSearch = (__exports__);
+
+/***/ }),
+
+/***/ 74928:
+/***/ ((module, __unused_webpack___webpack_exports__, __webpack_require__) => {
+
+/* harmony import */ var _node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(87537);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(23645);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);
+// Imports
+
+
+var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));
+// Module
+___CSS_LOADER_EXPORT___.push([module.id, "@media screen and (max-width:780px){.ctrld[data-v-1e438f06]{display:none}.input-search[data-v-1e438f06],.logo-wrapper[data-v-1e438f06]{display:block;text-align:center}.input-search[data-v-1e438f06]{margin-left:0}.logo-wrapper[data-v-1e438f06]{float:none;height:80px;margin:0 auto}}@media screen and (min-width:780px){.logo-wrapper[data-v-1e438f06]{float:left}.input-search[data-v-1e438f06]{display:block;padding-left:60px;margin-left:200px}}.input-search-wrap[data-v-1e438f06]{padding:20px 0;font-size:12px;direction:ltr}.logo-wrapper[data-v-1e438f06]{height:80px;width:200px;text-align:center}.logo-wrapper img[data-v-1e438f06]{border-radius:8px}.ingin-wrapper[data-v-1e438f06]{margin:0}.ingin-wrapper li[data-v-1e438f06]{display:inline-block;border-radius:5px 5px 0 0;list-style:none;padding:0 12px;margin:0 12px;cursor:pointer}.slected[data-v-1e438f06]{color:#fff;background:#143bd0}button[data-v-1e438f06],input[data-v-1e438f06]{height:35px;line-height:35px;outline:0;vertical-align:middle;border:1px solid #143bd0;font-size:16px;border-radius:4px 0 0 4px}.input-search-wrap .input-search input[data-v-1e438f06]{width:60%;box-sizing:border-box;vertical-align:middle;padding:0 14px;outline:0;margin-left:1px}.input-search-wrap .input-search button[data-v-1e438f06]{padding:0 14px;margin:-1px;border-radius:0 4px 4px 0;font-weight:700;background-color:#fff;color:#fff;background:#143bd0}.input-search-wrap .input-search button[data-v-1e438f06]:hover{cursor:pointer}.heiden[data-v-1e438f06]{display:none}", "",{"version":3,"sources":["webpack://./docs/.vuepress/components/InputSearch.vue","<no source>"],"names":[],"mappings":"AAEA,oCACE,wBACE,YACF,CCLF,8DDWI,aAAc,CACd,iBAAkB,CANpB,+BACE,aACF,CACA,+BAME,UAAW,CACX,WAAY,CACZ,aAJF,CAMF,CAEA,oCACE,+BACE,UACF,CACA,+BACE,aAAc,CACd,iBAAkB,CAClB,iBACF,CACF,CACA,oCACE,cAAe,CACf,cAAe,CACf,aACF,CACA,+BACE,WAAY,CACZ,WAAY,CACZ,iBACF,CACA,mCACE,iBACF,CAEA,gCACE,QACF,CACA,mCACE,oBAAqB,CAGrB,yBAA0B,CAC1B,eAAgB,CAChB,cAAe,CACf,aAAc,CACd,cACF,CAEA,0BACE,UAAW,CACX,kBACF,CAEA,+CAEE,WAAY,CACZ,gBAAiB,CACjB,SAAa,CACb,qBAAsB,CACtB,wBAAyB,CACzB,cAAe,CACf,yBACF,CACA,wDACE,SAAU,CACV,qBAAsB,CACtB,qBAAsB,CACtB,cAAe,CACf,SAAa,CACb,eACF,CACA,yDACE,cAAe,CACf,WAAY,CACZ,yBAA0B,CAC1B,eAAiB,CACjB,qBAAsB,CACtB,UAAW,CACX,kBACF,CACA,+DACE,cACF,CACA,yBACE,YACF","sourcesContent":["\n/* 小于780px */\n@media screen and (max-width: 780px) {\n  .ctrld {\n    display: none;\n  }\n  .input-search {\n    margin-left: 0;\n  }\n  .input-search,\n  .logo-wrapper {\n    display: block;\n    text-align: center;\n  }\n  .logo-wrapper {\n    float: none;\n    height: 80px;\n    margin: 0 auto;\n  }\n}\n/* 大于 780px */\n@media screen and (min-width: 780px) {\n  .logo-wrapper {\n    float: left;\n  }\n  .input-search {\n    display: block;\n    padding-left: 60px;\n    margin-left: 200px;\n  }\n}\n.input-search-wrap {\n  padding: 20px 0;\n  font-size: 12px;\n  direction: ltr;\n}\n.logo-wrapper {\n  height: 80px;\n  width: 200px;\n  text-align: center;\n}\n.logo-wrapper img {\n  border-radius: 8px;\n}\n\n.ingin-wrapper {\n  margin: 0;\n}\n.ingin-wrapper li {\n  display: inline-block;\n  margin: 0;\n  padding: 0;\n  border-radius: 5px 5px 0 0;\n  list-style: none;\n  padding: 0 12px;\n  margin: 0 12px;\n  cursor: pointer;\n}\n\n.slected {\n  color: #fff;\n  background: #143bd0;\n}\n\ninput,\nbutton {\n  height: 35px;\n  line-height: 35px;\n  outline: none;\n  vertical-align: middle;\n  border: 1px solid #143bd0;\n  font-size: 16px;\n  border-radius: 4px 0 0 4px;\n}\n.input-search-wrap .input-search input {\n  width: 60%;\n  box-sizing: border-box;\n  vertical-align: middle;\n  padding: 0 14px;\n  outline: none;\n  margin-left: 1px;\n}\n.input-search-wrap .input-search button {\n  padding: 0 14px;\n  margin: -1px;\n  border-radius: 0 4px 4px 0;\n  font-weight: bold;\n  background-color: #fff;\n  color: #fff;\n  background: #143bd0;\n}\n.input-search-wrap .input-search button:hover {\n  cursor: pointer;\n}\n.heiden {\n  display: none;\n}\n",null],"sourceRoot":""}]);
+// Exports
+/* unused harmony default export */ var __WEBPACK_DEFAULT_EXPORT__ = ((/* unused pure expression or super */ null && (___CSS_LOADER_EXPORT___)));
+
+
+/***/ }),
+
+/***/ 83744:
+/***/ ((__unused_webpack_module, exports) => {
+
+var __webpack_unused_export__;
+
+__webpack_unused_export__ = ({ value: true });
+// runtime helper for setting properties on components
+// in a tree-shakable way
+exports.Z = (sfc, props) => {
+    const target = sfc.__vccOpts || sfc;
+    for (const [key, val] of props) {
+        target[key] = val;
+    }
+    return target;
+};
+
+
+/***/ }),
+
+/***/ 46846:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+module.exports = __webpack_require__.p + "assets/img/kz321.bc98f404.png";
+
+/***/ })
+
+};
+;
+//# sourceMappingURL=915.app.js.map
